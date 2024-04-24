02edbea837c1fd74da79f45396910d1253dea6f9
Esther Nehemiah <105179203+CheeryCodes@users.noreply.github.com>
2024-04-24 04:05:39 -0700

    Create my_upcase.c

---
+++ my_upcase.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+char* my_upcase(char* param_1)
+{
+  char* result = param_1;
+  while(*param_1 != '\0'){
+      if (*param_1 >= 'a' && *param_1 <= 'z'){
+          *param_1 = *param_1 - 32;
+      }
+      param_1++;
+  }
+  return result;
+}
